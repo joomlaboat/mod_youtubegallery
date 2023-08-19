23c7cda46edd300a0a6ab872ef93185b651f98db
JoomlaBoat <ivankomlev@gmail.com>
2023-08-19 16:15:24 -0500

    Version Changed YG Component 5.5.2 compatible.

--- mod_youtubegallery.php
+++ mod_youtubegallery.php
@@ -9,78 +9,66 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-$path=JPATH_SITE.DIRECTORY_SEPARATOR.'components'.DIRECTORY_SEPARATOR.'com_youtubegallery'.DIRECTORY_SEPARATOR.'libraries'.DIRECTORY_SEPARATOR.'youtubegallery'.DIRECTORY_SEPARATOR;
-require_once($path.'loader.php');
+$path = JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_youtubegallery' . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'youtubegallery' . DIRECTORY_SEPARATOR;
+require_once($path . 'loader.php');
 YGLoadClasses();
 
 use YouTubeGallery\Helper;
 
-$listid=(int)$params->get( 'listid' );
+$listId = (int)$params->get('listid');
 
 //Get Theme
-if(Helper::check_user_agent('mobile'))
-{
-	//Use Mobile Theme if set.
-	$themeid=(int)$params->get( 'mobilethemeid' );
-	if($themeid==0)
-	    $themeid=(int)$params->get( 'themeid' );
-}
-else
-	$themeid=(int)$params->get( 'themeid' );
+if (CustomTables\Environment::check_user_agent('mobile')) {
+    //Use Mobile Theme if set.
+    $themeId = (int)$params->get('mobilethemeid');
+    if ($themeId == 0)
+        $themeId = (int)$params->get('themeid');
+} else
+    $themeId = (int)$params->get('themeid');
 
-$align='';
+$align = '';
 
-if($listid!=0 and $themeid!=0)
-{
-	$ygDB=new YouTubeGalleryDB;
+if ($listId != 0 and $themeId != 0) {
+    $ygDB = new YouTubeGalleryDB;
 
-	$videolist_and_theme_found=true;
+    $videoListAndThemeFound = true;
+    $videoListAndThemeFound=$ygDB->getVideoListTableRow($listId);
 
-	if(!$ygDB->getVideoListTableRow($listid))
-	{
-		JFactory::getApplication()->enqueueMessage(JText::_( 'MOD_YOUTUBEGALLERY_ERROR_VIDEOLIST_NOT_SET' ), 'error');
-		$videolist_and_theme_found=false;
-	}
+    if (!$videoListAndThemeFound)
+        JFactory::getApplication()->enqueueMessage(JText::_('MOD_YOUTUBEGALLERY_ERROR_VIDEOLIST_NOT_SET'), 'error');
 
-	if(!$ygDB->getThemeTableRow($themeid))
-	{
-		JFactory::getApplication()->enqueueMessage(JText::_( 'MOD_YOUTUBEGALLERY_ERROR_THEME_NOT_SET' ), 'error');
-		$videolist_and_theme_found=false;
-	}
-	
-	$youtubegallerycode = '';
+    $videoListAndThemeFound=$ygDB->getThemeTableRow($themeId);
+    if($videoListAndThemeFound)
+        JFactory::getApplication()->enqueueMessage(JText::_('MOD_YOUTUBEGALLERY_ERROR_THEME_NOT_SET'), 'error');
 
-	if($videolist_and_theme_found)
-	{
+    $youtubeGalleryCode = '';
 
-		$firstvideo='';
-		$youtubegallerycode='';
-		$total_number_of_rows=0;
+    if ($videoListAndThemeFound) {
 
-		$ygDB->update_playlist();
+        $firstVideo = '';
+        $total_number_of_rows = 0;
+        $ygDB->update_playlist();
 
-		$videoid=JFactory::getApplication()->input->getCmd('videoid','');
-		if(!isset($videoid) or $videoid=='')
-		{
-			$video=JFactory::getApplication()->input->getVar('video','');
-			$video=preg_replace('/[^a-zA-Z0-9-_]+/', '', $video);
+        $videoId = JFactory::getApplication()->input->getCmd('videoid', '');
+        if (!isset($videoId) or $videoId == '') {
+            $video = JFactory::getApplication()->input->getVar('video', '');
+            $video = preg_replace('/[^a-zA-Z0-9-_]+/', '', $video);
 
-			if($video!='')
-				$videoid=YouTubeGalleryDB::getVideoIDbyAlias($video);
-		}
+            if ($video != '')
+                $videoId = YouTubeGalleryDB::getVideoIDbyAlias($video);
+        }
 
-		if($ygDB->theme_row->es_playvideo==1 and $videoid!='')
-			$ygDB->theme_row->es_autoplay=1;
+        if ($ygDB->theme_row->es_playvideo == 1 and $videoId != '')
+            $ygDB->theme_row->es_autoplay = 1;
 
-		$videoid_new=$videoid;
-		$jinput=JFactory::getApplication()->input;
-		if($jinput->getInt('yg_api')==1)
-        {
-			$videolist=$ygDB->getVideoList_FromCache_From_Table($videoid_new,$total_number_of_rows,false);
-            $result=json_encode($videolist);
+        $videoIdNew = $videoId;
+        $jinput = JFactory::getApplication()->input;
+        if ($jinput->getInt('yg_api') == 1) {
+            $videoList = $ygDB->getVideoList_FromCache_From_Table($videoIdNew, $total_number_of_rows, false);
+            $result = json_encode($videoList);
 
             if (ob_get_contents())
-				ob_end_clean();
+                ob_end_clean();
 
             header('Content-Disposition: attachment; filename="youtubegallery_api.json"');
             header('Content-Type: application/json; charset=utf-8');
@@ -91,76 +79,60 @@ if($listid!=0 and $themeid!=0)
             die;
 
             return '';
-		}
-        else
-		{
-			$videolist=$ygDB->getVideoList_FromCache_From_Table($videoid_new,$total_number_of_rows);
-		}
-		
+        } else {
+            $videoList = $ygDB->getVideoList_FromCache_From_Table($videoIdNew, $total_number_of_rows);
+        }
 
-		if($videoid=='')
-		{
-			if($ygDB->theme_row->es_playvideo==1 and $videoid_new!='')
-				$videoid=$videoid_new;
-		}
+        $custom_itemid = (int)$params->get('customitemid');
+        $renderer = new YouTubeGalleryRenderer;
+        $galleryModule = $renderer->render(
+            $videoList,
+            $ygDB->videoListRow,
+            $ygDB->theme_row,
+            $total_number_of_rows,
+            $videoId,
+            $custom_itemid
+        );
 
-		$custom_itemid=(int)$params->get( 'customitemid' );
+        if ($params->get('allowcontentplugins')) {
+            $o = new stdClass();
+            $o->text = $galleryModule;
 
-		$renderer= new YouTubeGalleryRenderer;
+            $dispatcher = JDispatcher::getInstance();
 
-		$gallerymodule=$renderer->render(
-			$videolist,
-			$ygDB->videolist_row,
-			$ygDB->theme_row,
-			$total_number_of_rows,
-			$videoid,
-			$custom_itemid
-		);
+            JPluginHelper::importPlugin('content');
 
-		if($params->get( 'allowcontentplugins' ))
-		{
-			$o = new stdClass();
-			$o->text=$gallerymodule;
+            $r = $dispatcher->trigger('onContentPrepare', array('com_content.article', &$o, &$params_, 0));
 
-			$dispatcher	= JDispatcher::getInstance();
+            $galleryModule = $o->text;
+        }
 
-			JPluginHelper::importPlugin('content');
+        $align = $params->get('galleryalign');
 
-			$r = $dispatcher->trigger('onContentPrepare', array ('com_content.article', &$o, &$params_, 0));
+        switch ($align) {
+            case 'left' :
+                $youtubeGalleryCode .= '<div style="float:left;position:relative;">' . $galleryModule . '</div>';
+                break;
 
-			$gallerymodule=$o->text;
-		}
+            case 'center' :
+                $youtubeGalleryCode .= '<div style="width:' . $ygDB->theme_row->width . 'px;margin-left:auto;margin-right:auto;position:relative;">' . $galleryModule . '</div>';
+                break;
 
-		$align=$params->get( 'galleryalign' );
+            case 'right' :
+                $youtubeGalleryCode .= '<div style="float:right;position:relative;">' . $galleryModule . '</div>';
+                break;
 
-		switch($align)
-		{
-		   	case 'left' :
-		   		$youtubegallerycode.= '<div style="float:left;position:relative;">'.$gallerymodule.'</div>';
-			break;
+            default :
+                $youtubeGalleryCode .= $galleryModule;
+                break;
 
-			case 'center' :
-		   		$youtubegallerycode.= '<div style="width:'.$ygDB->theme_row->width.'px;margin-left:auto;margin-right:auto;position:relative;">'.$gallerymodule.'</div>';
-			break;
+        }//switch($align)
+    } else {
+        //JFactory::getApplication()->enqueueMessage('Youtube Gallery: Video List and Theme not found.', 'error');
+        //echo '<p style="background-color:red;color:white;">Youtube Gallery: Video List and Theme not found.</p>';
+    }
 
-		   	case 'right' :
-		  		$youtubegallerycode.= '<div style="float:right;position:relative;">'.$gallerymodule.'</div>';
-			break;
+    echo $youtubeGalleryCode;
 
-		   	default :
-		   		$youtubegallerycode.= $gallerymodule;
-			break;
-
-		}//switch($align)
-	}
-	else
-	{
-		//JFactory::getApplication()->enqueueMessage('Youtube Gallery: Video List and Theme not found.', 'error');
-		//echo '<p style="background-color:red;color:white;">Youtube Gallery: Video List and Theme not found.</p>';
-	}
-
-	echo $youtubegallerycode;
-
-}
-else
-	echo '<p>Video list or Theme not selected</p>';
+} else
+    echo '<p>Video list or Theme not selected</p>';
